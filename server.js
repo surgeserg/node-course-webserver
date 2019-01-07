06d8cd15de63bd5d42b334d5144be4d9481ab3b3
Sergio Sanchez <surgeserg@hotmail.com>
2019-01-07 12:28:52 -0600

    Added projects URL for portfolio. (2)

--- server.js
+++ server.js
@@ -54,6 +54,13 @@ app.get('/bad', (req, res) => {
 	});
 });
 
+app.get('/projects', (req, res) => {
+	res.render('projects.hbs', {
+		head: 'Projects'
+	});
+
+});
+
 
 app.listen(port, () => {
 	console.log(`Server up on port ${port}`);
